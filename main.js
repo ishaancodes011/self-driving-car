76b21991883b4583de49e9c4ed5ee307c3989a1e
ishaancodes011 <ishaancodes011@gmail.com>
2023-10-10 16:13:58 +0530

    Trial Final Code

--- main.js
+++ main.js
@@ -8,14 +8,41 @@ const networkCtx = networkCanvas.getContext('2d');
 
 const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
 
-const N = 100;
+const N = 1;
 const cars = generateCars(N);
+let bestCar = cars[0];
+fetch("bestBrain.json").then(res => res.json()).then((data)=>{const bB = data})
+if(bB){
+    for (let i=0; i<cars.length; i++){
+        cars[i].brain = JSON.parse("bestBrain.json");
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain, 0.2);
+        }
+    }
+}
 
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2, getRandomColor())
 ];
 
 animate();
+
+function save() {
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(new Blob([JSON.stringify(bestCar.brain)], {type: "JSON"}))
+    a.setAttribute("download", "bestBrain.json");
+    a.click();
+}
+
+function discard() {
+    localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N) {
     const cars = [];
@@ -32,7 +59,7 @@ function animate(time) {
     for (let i=0; i<cars.length; i++) {
         cars[i].update(road.borders, traffic);
     }
-    const bestCar = cars.find(c => c.y==Math.min(...cars.map(c => c.y)));
+    bestCar = cars.find(c => c.y==Math.min(...cars.map(c => c.y)));
 
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
@@ -57,4 +84,9 @@ function animate(time) {
 
     Visualizer.drawNetwork(networkCtx, bestCar.brain);
     requestAnimationFrame(animate);
+}
+
+function getRandomColor() {
+    const hue = 290 + Math.random()*260;
+    return "hsl(" + hue + ", 100%, 60%)";
 }
